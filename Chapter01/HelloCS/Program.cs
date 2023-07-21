4abe302fb0b3b67373badf12d38e2705ff3a9692
若寒 <tf320621@163.com>
2023-07-21 23:02:25 +0800

    feat(类型): 基础数据类型和引用类型等

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -31,3 +31,17 @@ System.Data.DataSet ds;
         arg2:r.Name
     );
 }
+
+// 定义一些常见的变量
+uint naturalNumber = 23;
+int integerNumber = -23;
+float realNumber = 2.3F;
+double anotherRealNumber = 2.3D;
+
+//测试常见的二进制，十六进制转换
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+Console.WriteLine($"decimalNotation={decimalNotation},binaryNotation={binaryNotation},hexadecimalNotation={hexadecimalNotation}");
+
