afc695bcd4674c052e12b32ede865b1e5f363c43
若寒 <tf320621@163.com>
2023-08-02 00:44:31 +0800

    docs(C#入门经典): 第70页

---
+++ Chapter03/WhileAndDoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using static System.Convert;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// while
+int i = 10;
+while(i> 1){
+    WriteLine($"执行{i}次");
+
+    i--;
+}
+
+// dowhile
+// int count = 10;
+// string pa = string.Empty;
+// do
+// {
+//     WriteLine($"第{count}次输入密码");
+//     pa = ReadLine();
+// } while (pa == "111");
+
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g:{g},h:{h}");
