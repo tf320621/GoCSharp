20309af8d9c8f577302887583678a1bdaf13ee6c
若寒 <tf320621@163.com>
2023-08-03 00:25:20 +0800

    docs(C#入门经典): 第81页

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+using static System.Convert;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//检测溢出和不检测溢出
+// checked
+// {
+//     int x = int.MaxValue-1;
+//     Console.WriteLine($"Initial value:{x}");
+//     x++;
+
+//     Console.WriteLine($"After incrementing:{x}");
+//     x++;
+
+//     Console.WriteLine($"After incrementing:{x}");
+//     x++;
+
+//     Console.WriteLine($"After incrementing:{x}");
+//     x++;
+// }
+//int z = int.MaxValue+1;
+
+// unchecked
+// {
+//     int y = int.MaxValue+1;
+// }
+
+// for(;true;){
+
+// }
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i%3==0 && i%5==0)
+    {
+        Console.Write("fizzbuzz,");
+        continue;
+    }
+    if(i%3==0){
+        Console.Write("fizz,");
+        continue;
+    }
+    if (i%5 ==0)
+    {
+        Console.Write("buzz,");
+        continue;
+    }
+    Console.Write(i+",");
+}
+
+string a = ReadLine();
+string b = ReadLine();
--- Chapter03/WhileAndDoWhile/Program.cs
+++ Chapter03/WhileAndDoWhile/Program.cs
@@ -24,3 +24,27 @@ while(i> 1){
 double g = 9.8;
 int h = ToInt32(g);
 WriteLine($"g:{g},h:{h}");
+
+// C# 特有圆整，银行家圆整法
+double [] doubles = new[]{9.49,9.5,9.51,10.49,10.5,10.51};
+foreach (var item in doubles)
+{
+    WriteLine($"ToInt({item} is {ToInt32(item)})");
+}
+
+// 改变圆整策略，变成普通圆整法。
+foreach (var item in doubles)
+{
+    WriteLine(format:
+     "Math.Round({0},0,MidpointRounding.AwayFromZero) is {1}",
+     arg0:item,
+     arg1:Math.Round(value:item,digits:0,mode:MidpointRounding.AwayFromZero)
+    );
+}
+
+// Math.Round(9.49,0,MidpointRounding.AwayFromZero) is 9
+// Math.Round(9.5,0,MidpointRounding.AwayFromZero) is 10
+// Math.Round(9.51,0,MidpointRounding.AwayFromZero) is 10
+// Math.Round(10.49,0,MidpointRounding.AwayFromZero) is 10
+// Math.Round(10.5,0,MidpointRounding.AwayFromZero) is 11
+// Math.Round(10.51,0,MidpointRounding.AwayFromZero) is 11
