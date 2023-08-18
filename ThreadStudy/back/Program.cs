3e676470e61d331537d99c22aa9bbeecd7e9c501
若寒 <tf320621@163.com>
2023-08-18 17:27:23 +0800

    11

---
+++ ThreadStudy/back/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static void PrintNumbers(){
+    Console.WriteLine("strating......");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+}
+
+
+Thread t = new Thread(PrintNumbers);
+t.Strat():
+
+PrintNumbers():
