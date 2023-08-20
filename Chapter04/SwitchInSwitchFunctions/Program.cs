8bd0149e72c0493996a044bc7ba6ea555c81a7c2
若寒 <tf320621@163.com>
2023-08-20 19:25:08 +0800

    docs(C#入门经典): 第87页

---
+++ Chapter04/SwitchInSwitchFunctions/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 编写switch嵌套switch
+
+static string CardinalToOrdinal(int number){
+    switch (number)
+    {
+        case 11:
+        case 12:
+        case 13:
+        return $"{number}th";
+        default:
+        int lastDigit = number%10;
+        string suffix = lastDigit switch{
+            1=>"st",
+            2=>"nd",
+            3=>"rd",
+            _=>"th"
+        };
+        return $"{number}{suffix}";
+    }
+}
+
+bool isNumber;
+do
+{
+    isNumber = int.TryParse(ReadLine(),out int numb);
+    if (isNumber)
+    {
+       var ret =  CardinalToOrdinal(numb);
+       WriteLine(ret);
+    }else{
+        WriteLine("hehe");
+    }
+} while (isNumber);
---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// 静态方法
+static void TimesTable(byte number){
+    WriteLine($"1111111111");
+
+    for (int i = 1; i < number; i++){
+        WriteLine($"row:{i} * number{number} = {i*number}");
+        
+    }
+}
+
+static void RunTimesTable(){
+    bool isNumber;
+    do
+    {
+        Write("请输入0-255之间的数字");
+        isNumber = byte.TryParse(ReadLine(),out byte number);
+
+        if (isNumber)
+        {
+            TimesTable(number);
+        }else{
+            WriteLine("hehe");
+        }
+    } while (isNumber);
+}
+
+RunTimesTable();
+
