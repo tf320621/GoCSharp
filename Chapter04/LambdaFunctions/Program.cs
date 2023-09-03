f407fc587c7f19d0ddb5edcc8942eec43bb66e58
若寒 <tf320621@163.com>
2023-09-03 14:55:11 +0800

    feat(调试代码方法): 第96页

---
+++ Chapter04/LambdaFunctions/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static double Add(double a, double b){
+    return a + b;
+}
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+Console.WriteLine($"{a}+{b}={answer}");
+
