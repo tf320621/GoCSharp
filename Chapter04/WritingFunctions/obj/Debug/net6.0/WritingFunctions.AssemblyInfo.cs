fcf62263c16270d069f708422e45dbe95834eaac
NikolaSugar._ <tf320621@163.com>
2024-02-08 21:18:24 +0800

    feat(调试代码方法): 第42页 处理空值问题处理

--- Chapter04/WritingFunctions/obj/Debug/net6.0/WritingFunctions.AssemblyInfo.cs
+++ Chapter04/WritingFunctions/obj/Debug/net6.0/WritingFunctions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WritingFunctions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f407fc587c7f19d0ddb5edcc8942eec43bb66e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WritingFunctions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WritingFunctions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
